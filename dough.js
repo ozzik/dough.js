2a0dbd4ddc4ffd2a4710c2d9526092b4c3bb0966
Oz Pinhas <hey@ozzik.co>
2014-05-27 22:33:10 +0300

    - Adds transform methods + browser engine detection - Adds tap/click event method - Fixes bad classList fallback implementation - Prettifies some code

--- dough.js
+++ dough.js
@@ -7,110 +7,124 @@
 
 	/* ==== Private shenanigans ==== */
 	var _listeners = {}, // Saved event listeners methods
-		_crumbs = 1, // Unique ID index (given to every DOM element for identifying it in listeners object)
-		_htmlDivElement = document.createElement("div"), // Dummy div element for HTML creation
-		_core;
+	    _crumbs = 1, // Unique ID index (given to every DOM element for identifying it in listeners object)
+	    _htmlDivElement = document.createElement("div"), // Dummy div element for HTML creation
+	    _renderEngine,
+	    _eventPointerStart,
+	    _eventPointerMove,
+	    _eventPointerEnd;
 
-	_core = {
-		/* ==== Core methods ==== */
-		/* Retrieves elements from DOM into a Dough object  */
-		find: function(selector, base) {
-			var dough = new Dough(), // Dough object to be returned
-				e = [];
+	/* ==== Core methods ==== */
+    /* Retrieves elements from DOM into a Dough object  */
+    function _find(selector, base) {
+        var dough = new Dough(), // Dough object to be returned
+            e = [];
 
-			// Someday there will be an efficient query method selection
-			// (keep in mind: /^#[\w-]+$/i.test(selector))
-			if (typeof selector === "string") {
-				e = (base || document).querySelectorAll(selector);
-			} else if (selector instanceof NodeList || Array.isArray(selector)) {
-				e = selector;
-			} else if (selector instanceof HTMLElement) {
-				e[0] = selector;
-			}
+        // Someday there will be an efficient query method selection
+        // (keep in mind: /^#[\w-]+$/i.test(selector))
+        if (typeof selector === "string") {
+            e = (base || document).querySelectorAll(selector);
+        } else if (selector instanceof NodeList || Array.isArray(selector)) {
+            e = selector;
+        } else if (selector instanceof HTMLElement) {
+            e[0] = selector;
+        }
 
-			// Storing elements in dough object as keys (=elements collection)
-			for (var i = 0, len = e.length; i < len; i++) {
-				dough[i] = e[i];
-			}
-			dough.length = e.length;
+        // Storing elements in dough object as keys (=elements collection)
+        for (var i = 0, len = e.length; i < len; i++) {
+            dough[i] = e[i];
+        }
+        dough.length = e.length;
 
-			return dough;
-		},
+        return dough;
+    }
 
-		/* ==== DOM methods ==== */
-		each: function(e, fn, args) {
-			for (var i = 0; i < e.length; i++) {
-				fn(e[i], args);
-			}
-		},
+    function _each(e, fn, args) {
+        for (var i = 0; i < e.length; i++) {
+            fn(e[i], args);
+        }
+    }
 
-		/* ==== CSS-related methods ==== */
-		/* Mingles with the CSS selector class(es) of every element in the collection */
-		/* Fallback method is defined later on */
-		edit_classes: function(e, action, classes) {
-			classes = classes.split(" ");
+    /* ==== CSS-related methods ==== */
+    /* Mingles with the CSS selector class(es) of every element in the collection */
+    /* Fallback method is defined later on */
+    function _edit_classes(e, action, classes) {
+        classes = classes.split(" ");
 
-			for (var i = 0; i < e.length; i++) {
-				for (var j = 0, jLen = classes.length; j < jLen; j++) {
-					e[i].classList[action](classes[j]);
-				}
-			}
-		},
+        for (var i = 0; i < e.length; i++) {
+            for (var j = 0, jLen = classes.length; j < jLen; j++) {
+                e[i].classList[action](classes[j]);
+            }
+        }
+    }
 
-		/* Parses a given HTML string to DOM elements */
-		parse_content: function(content) {
-			var childNodes,
-				returnedNodes = [];
+    /* Parses a given HTML string to DOM elements */
+    function _parse_content(content) {
+        var childNodes,
+            returnedNodes = [];
 
-			if (content instanceof HTMLElement) {
-				return [content];
-			} else {
-				_htmlDivElement.innerHTML = content;
-				childNodes = _htmlDivElement.childNodes;
+        if (content instanceof HTMLElement) {
+            return [content];
+        } else {
+            _htmlDivElement.innerHTML = content;
+            childNodes = _htmlDivElement.childNodes;
 
-				for (var i = 0, len = childNodes.length; i < len; i++) {
-					returnedNodes[i] = childNodes[i];
-				}
+            for (var i = 0, len = childNodes.length; i < len; i++) {
+                returnedNodes[i] = childNodes[i];
+            }
 
-				return returnedNodes;
-			}
-		},
+            return returnedNodes;
+        }
+    }
 
-		/* Gets the sibling node element of a given element */
-		get_sibling: function(e, isNext) {
-			var property = (isNext ? "next" : "previous") + "Sibling",
-				sibling = e[property];
+    /* Gets the sibling node element of a given element */
+    function _get_sibling(e, isNext) {
+        var property = (isNext ? "next" : "previous") + "Sibling",
+            sibling = e[property];
 
-			while (sibling && sibling.nodeType !== Node.ELEMENT_NODE) {
-				sibling = sibling[property];
-			}
+        while (sibling && sibling.nodeType !== Node.ELEMENT_NODE) {
+            sibling = sibling[property];
+        }
 
-			return sibling;
-		},
+        return sibling;
+    }
 
-		/* Inserts an HTTP parameter and its value to a given string */
-		add_http_parameter: function(data, parameter, value) {
-			return (data ? data + "&" : "") + parameter + "=" + value;
-		},
+    /* Inserts an HTTP parameter and its value to a given string */
+    function _add_http_parameter(data, parameter, value) {
+        return (data ? data + "&" : "") + parameter + "=" + value;
+    }
 
-		/* Creates an HTTP parameters string based on a given parameters object */
-		generate_http_parameters: function(data) {
-			var dataKeys = Object.keys(data),
-				params = "";
+    /* Creates an HTTP parameters string based on a given parameters object */
+    function _generate_http_parameters(data) {
+        var dataKeys = Object.keys(data),
+            params = "";
 
-			for (var i = 0, len = dataKeys.length; i < len; i++) {
-				params = _core.add_http_parameter(params, dataKeys[i], data[dataKeys[i]]);
-			}
+        for (var i = 0, len = dataKeys.length; i < len; i++) {
+            params = _add_http_parameter(params, dataKeys[i], data[dataKeys[i]]);
+        }
 
-			return params;
-		}
-	};
+        return params;
+    }
 
-	// Logging and such
-	function log(str, isBadUsage) {
-		str += (isBadUsage) ? " god damned it!" : "";
-		console.warn ? console.warn(str) : console.log(str);
-	}
+    /* Internal message printing */
+    function _log(str, isBadUsage) {
+        str += (isBadUsage) ? " god damned it!" : "";
+        console.warn ? console.warn(str) : console.log(str);
+    }
+
+    /* Identifies the browser's rendering engine */
+    function _detect_engine() {
+        var ua = navigator.userAgent,
+            value = "";
+
+        if (ua.indexOf("webkit")) {
+            value = "webkit";
+        } else if (ua.indexOf("Firefox")) {
+            value = "moz";
+        }
+
+        return value;
+    }
 
 	/* ==== Exposed thingies ==== */
 	// Exposed methods
@@ -119,41 +133,42 @@
 		/* Retrieves descendant element(s) as a new Dough object */
 		/* (from the first element in the collection) */
 		find: function(selector) {
-			return _core.find(selector, this[0]);
+			return _find(selector, this[0]);
 		},
 
 		/* Retrieves the successor element to an element */
 		/* (relative to the first element in the collection) */
 		next: function() {
-			return doughFn(_core.get_sibling(this[0], true));
+			return doughFn(_get_sibling(this[0], true));
 		},
 
 		/* Retrieves the predecessor element of an element */
 		/* (relative to the first element in the collection) */
 		previous: function() {
-			return doughFn(_core.get_sibling(this[0]));
+			return doughFn(_get_sibling(this[0]));
 		},
 
 		/* ==== Style methods ==== */
 		/* Adds a CSS selector class(es) to the collection's elements */
 		addClass: function(classes) {
-			_core.edit_classes(this, "add", classes.trim());
+			_edit_classes(this, "add", classes.trim());
 
 			return this;
 		},
 
 		/* Removes a CSS selector class(es) from the collection's elements */
 		removeClass: function(classes) {
-			_core.edit_classes(this, "remove", classes.trim());
+			_edit_classes(this, "remove", classes.trim());
 
 			return this;
 		},
 
 		/* Adds/removes a CSS selector class(es) from/to collection's elements */
-		toggleClass: function(classes) {
-			_core.edit_classes(this, "toggle", classes.trim());
+		toggleClass: function(classes, isAdd) {
+		    var action = (isAdd === undefined) ? "toggle" : (isAdd ? "add" : "remove");
+		    _edit_classes(this, action, classes.trim());
 
-			return this;
+		    return this;
 		},
 
 		/* Determines whether an element has a given CSS selector class(es) */
@@ -196,6 +211,27 @@
             return this;
 		},
 
+		/* Sets the CSS transform property of the collection's elements */
+		transform: function(transformFn) {
+		    for (var i = 0; i < this.length; i++) {
+		        this[i].style[_renderEngine + "Transform"] = transformFn;
+		    }
+		},
+
+		/* Sets a CSS translate3d method to the collection's elements */
+		translate: function(x, y, z) {
+		    for (var i = 0; i < this.length; i++) {
+		        this[i].style[_renderEngine + "Transform"] = "translate3d(" + (x || 0) + "px," + (y || 0) + "px," + (z || 0) + "px)";
+		    }
+		},
+
+		/* Removes a CSS translate3d method from the collection's elements */
+		reset_translate: function() {
+		    for (var i = 0; i < this.length; i++) {
+		        this[i].style[_renderEngine + "Transform"] = "";
+		    }
+		},
+
 		/* ==== Structure methods ==== */
 		/* Sets or removes an attribute to/from collection's elements */
 		/* (Notice: not passing a value will trigger removal of the attribute) */
@@ -223,7 +259,7 @@
 
 		/* Appends an element before every element in the collection */
 		before: function(content) {
-			content = _core.parse_content(content);
+			content = _parse_content(content);
 
 			for (var i = 0; i < this.length; i++) {
 				for (var j = 0, jLen = content.length; j < jLen; j++) {
@@ -238,10 +274,10 @@
 		after: function(content) {
 			var successor;
 
-			content = _core.parse_content(content);
+			content = _parse_content(content);
 
 			for (var i = 0; i < this.length; i++) {
-				successor = _core.get_sibling(this[i], true);
+				successor = _get_sibling(this[i], true);
 
 				for (var j = 0, jLen = content.length; j < jLen; j++) {
 					if (successor) {
@@ -300,7 +336,48 @@
             return this;
 		},
 
+		/* ==== Action methods ==== */
+		/* Creates a click/tap event listener */
+		click: function(fn) {
+		    this.on(_eventPointerStart, function(e) {
+		        e.preventDefault();
+
+		        this._$e = $(this);
+		        this._startX = (_isTouch) ? e.touches[0].pageX : e.pageX;
+		        this._startY = (_isTouch) ? e.touches[0].pageY : e.pageY;
+		        this._didMove = false;
+		        this._isPointerDown = true;
+
+		        this._$e.addClass("pressed");
+		    });
+
+		    this.on(_eventPointerMove, function(e) {
+		        if (!this._isPointerDown) { return; }
+
+		        var x = (_isTouch) ? e.touches[0].pageX : e.pageX,
+		            y = (_isTouch) ? e.touches[0].pageY : e.pageY;
+
+		        this._didMove = (Math.abs(x - this._startX) >= 10) || (Math.abs(y - this._startY) >= 10);
+		        if (this._didMove) {
+		            this._$e.removeClass("pressed");
+		        }
+		    });
+
+		    this.on(_eventPointerEnd, function(e) {
+		        this._isPointerDown = false;
+
+		        if (!this._didMove) {
+		            this._$e.removeClass("pressed");
+
+		            fn(this._$e);
+		        }
+		    });
+
+		    return this;
+		},
+
 		/* ==== Collection methods ==== */
+		/* Executes a given method on the collection's elements */
 		each: function(fn) {
 			for (var i = 0; i < this.length; i++) {
 				fn(this[i]);
@@ -313,16 +390,14 @@
 	// Conditional methods
 	// No support for classList API -> using regex fallback
 	if (document.body['classList']) {
-		_core.edit_classes = function(e, action, classes) {
+		_edit_classes = function(e, action, classes) {
 			var regex,
-				removeRegex,
 				hasClass;
 			classes = classes.split(" ");
 
 			// Processing classes for each element
 			for (var i = 0, len = classes.length; i < len; i++) {
-				regex = new RegExp("\\b" + classes[i] + "\\b");
-				removeRegex = new RegExp("( \\b" + classes[i] + "\\b|\\b" + classes[i] + "\\b |^" + classes[i] + "$)");
+				regex = new RegExp("( \\b" + classes[i] + "\\b|\\b" + classes[i] + "\\b |^" + classes[i] + "$)");
 
 				for (var j = 0; j < e.length; j++) {
 					hasClass = regex.test(e[j].className);
@@ -331,7 +406,7 @@
 					if ((action === "add" || action === "toggle") && !hasClass) {
 						e[j].className += (e[j].className ? " " : "") + classes[i];
 					} else if ((action === "remove" || action === "toggle") && hasClass) {
-						e[j].className = e[j].className.replace(removeRegex, "");
+						e[j].className = e[j].className.replace(regex, "");
 					}
 				}
 			}
@@ -343,56 +418,83 @@
 			classes = classes.split(" ");
 
 			for (var i = 0, len = classes.length; i < len && hasClass; i++) {
-				regex = new RegExp("\\b" + classes[i] + "\\b");
+				regex = new RegExp("( \\b" + classes[i] + "\\b|\\b" + classes[i] + "\\b |^" + classes[i] + "$)");
 				hasClass = hasClass && regex.test(this[0].className);
 			}
 
 			return hasClass;
 		}
 	}
+	_renderEngine = _detect_engine();
+	_isTouch = "ontouchstart" in document;
+
+	_eventPointerStart = _isTouch ? "touchstart" : "mousedown";
+	_eventPointerMove = _isTouch ? "touchmove" : "mousemove";
+	_eventPointerEnd = _isTouch ? "touchend" : "mouseup";
 
 	// Static methods
+	doughFn.engine = _renderEngine;
+
+	doughFn.parseJSON = function(text) {
+	    var result = {
+	        isSuccess: false,
+	        json: null
+	    };
+
+	    try {
+	        result.json = JSON.parse(text);
+	        result.isSuccess = true;
+	    } catch (e) {
+	        result.error = e;
+	    }
+
+	    return result;
+	};
+
 	/* Performs an asynchronus HTTP request */
 	doughFn.ajax = function(options) {
-		var midget = new XMLHttpRequest(),
-			data = "",
-			isJSON = (options.contentType === undefined || options.contentType === "application/json");
+        var midget = new XMLHttpRequest(),
+            data = "",
+            isJSON = (options.contentType === undefined || options.contentType === "application/json");
 
-		options.type = options.type.toLowerCase();
-		options.contentType = (isJSON) ? "application/json" : options.contentType; // Setting deafult to JSON
-		options.contentType = (options.type !== "get") ? "application/x-www-form-urlencoded" : options.contentType;
-		options.charset = (options.charset) ? options.charset : "UTF-8";
+        options.type = options.type.toLowerCase();
+        options.contentType = (isJSON) ? "application/json" : options.contentType; // Setting deafult to JSON
+        options.contentType = (options.type !== "get") ? "application/x-www-form-urlencoded" : options.contentType;
+        options.charset = (options.charset) ? options.charset : "UTF-8";
 
-		midget.open(options.type, options.url, true);
-		midget.setRequestHeader("Content-Type", options.contentType + ";charset=" + options.charset);
+        midget.open(options.type, options.url, true);
+        midget.setRequestHeader("Content-Type", options.contentType + ";charset=" + options.charset);
 
-		midget.onreadystatechange = function() {
-			if (midget.readyState === 4) {
-				if (midget.status === 200) {
-					var json;
+        midget.onreadystatechange = function() {
+            if (midget.readyState === 4) {
+                if (midget.status === 200) {
+                    var result;
 
-					if (isJSON) {
-						try {
-							json = JSON.parse(midget.responseText);
-						} catch (e) {
-							options.parseError ? options.parseError(e) : log("Dough JSON parsing error: " + e.name + " - " + e.message + ". You should handle this error with a .parseError method", true);
-							return;
-						}
-					}
+                    if (isJSON) {
+                        result = doughFn.parseJSON(midget.responseText);
 
-					options.success((isJSON) ? json : midget.responseText); // Success
-				} else {
-					options.error ? options.error(midget.status) : log("Dough AJAX error: Returned " + midget.status + ". You should handle this error with a .error method", true);
-				}
-			}
-		};
+                        if (result.isSuccess) {
+                            result = result.json;
+                        } else {
+                            console.log(result);
+                            options.parseError ? options.parseError(result.e) : _log("Dough JSON parsing error: " + result.error.name + " - " + result.error.message + ". You should handle this error with a .parseError method", true);
+                            return;
+                        }
+                    }
 
-		if (typeof options.data === "object" && options.type !== "get") {
-			data = _core.generate_http_parameters(options.data);
-		}
+                    options.success((isJSON) ? result : midget.responseText); // Success
+                } else {
+                    options.error ? options.error(midget.status) : _log("Dough AJAX error: Returned " + midget.status + ". You should handle this error with a .error method", true);
+                }
+            }
+        };
 
-		midget.send(data);
-	};
+        if (typeof options.data === "object" && options.type !== "get") {
+            data = _generate_http_parameters(options.data);
+        }
+
+        midget.send(data);
+    };
 	// Programmatically creating HTTP-matching methods
 	var httpMethods = ["get", "post", "put", "delete"];
 
@@ -405,11 +507,32 @@
 		})(httpMethods[i]);
 	}
 
+	/* Gets a JSON file locally */
+	doughFn.getJSON = function(options) {
+	    doughFn.get({
+	        url: options.url,
+	        contentType: "text/plain",
+	        success: function(data) {
+	            // Network success
+	            var result = doughFn.parseJSON(data);
+
+	            if (result.isSuccess) {
+	                options.success(result.json);
+	            } else {
+	                (options.parseError) ? options.parseError(result.error) : _log("Dough JSON parsing error: " + result.error.name + " - " + result.error.message + ". You should handle this error with a .parseError method", true);
+	            }
+	        },
+	        error: function(error) {
+	            options.error ? options.error(error) : _log("Dough AJAX error: Returned " + error + ". You should handle this error with a .error method", true);
+	        }
+	    });
+	};
+
 	/* Performs a cross-site request by injecting the request as a script */
 	doughFn.jsonp = function(options) {
 		var script = document.createElement("script");
 
-		script.src = options.url + "?" + (options.callbackParam ? options.callbackParam : "callback") + "=" + options.callback + (options.data ? "&" + _core.generate_http_parameters(options.data) : "");
+		script.src = options.url + "?" + (options.callbackParam ? options.callbackParam : "callback") + "=" + options.callback + (options.data ? "&" + _generate_http_parameters(options.data) : "");
 		script.type = "text/javascript";
 
 		document.head.appendChild(script);
@@ -417,7 +540,7 @@
 
 	// Trigger function ($)
 	function doughFn(selector) {
-		return _core.find(selector);
+		return _find(selector);
 	}
 
 	window['$'] = (!window['$']) ? doughFn : window['$'];
